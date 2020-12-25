2c8b19b4ddcf117d2e6d86c4a8e5117de17aca0e
Shaafi <ishaafi2294@gmail.com>
2020-12-25 14:15:59 +0600

    Add class structure in trie

--- Trie.cpp
+++ Trie.cpp
@@ -1,58 +1,84 @@
-class data{
+class Node {
 public:
     bool endmark;
     int cnt;
-    data *node[10];
-    data(){
+    Node* node[10];
+    Node(){
         endmark=false;
         cnt = 0;
-        for(int i=0;i<10;i++)node[i]=NULL;
+        for(int i=0;i<10;i++) node[i]=NULL;
     }
 };
-data *root;
-void Insert(string s){
-    int len = s.size();
-    data *now = root;
-    for(int i = 0;i < len; i++){
-        if(now -> node[s[i]-'0'] == NULL) now->node[s[i]-'0'] = new data();
-        now=now -> node[s[i]-'0'];
-        now -> cnt += 1;
+class Trie {
+    Node *root;
+public:
+    Trie(){
+        root = new Node();
     }
-}
-ll query_min(string s){
-    data *now=root;
-    int len = s.size(), pw_id = len - 1;
-    ll ans=0LL;
-    for(int i = 0; i < len; i++){
-        int val = s[i];
-        if(now->node[val] != NULL && now -> node[val] -> cnt > 0){
-            now = now -> node[val];
+    
+    void insert(string &str) {
+        Node *cur = root;
+        for(char ch : str) {
+            if (cur -> arr[ch - 'a'] == NULL) cur -> arr[ch - 'a'] = new Node();
+            cur = cur -> arr[ch - 'a'];
+            cur -> count += 1;
         }
-        else{
-            ans += pw[pw_id]; val ^= 1;
-            now = now->node[val];
+    }
+    
+    string get_unique_prefix(string &str) {
+        string prefix = "";
+        Node *cur = root;
+        for(char ch : str) {
+            cur = cur -> arr[ch - 'a'];
+            prefix += ch;
+            if(cur -> count == 1) return prefix;
         }
-       /// now->cnt -= 1;///Use this if you need to delete
-        pw_id -= 1;
+        return prefix;
+    }
+
+    void Insert(string s){
+        int len = s.size();
+        data *now = root;
+        for(int i = 0;i < len; i++){
+            if(now -> node[s[i]-'0'] == NULL) now->node[s[i]-'0'] = new data();
+            now=now -> node[s[i]-'0'];
+            now -> cnt += 1;
+        }
+    }
+    ll query_min(string s){
+        data *now=root;
+        int len = s.size(), pw_id = len - 1;
+        ll ans=0LL;
+        for(int i = 0; i < len; i++){
+            int val = s[i];
+            if(now->node[val] != NULL && now -> node[val] -> cnt > 0){
+                now = now -> node[val];
+            }
+            else{
+                ans += pw[pw_id]; val ^= 1;
+                now = now->node[val];
+            }
+        /// now->cnt -= 1;///Use this if you need to delete
+            pw_id -= 1;
+        }
+        return ans;
+    }
+    ll query_max(string s){
+        data *now=root;
+        int len = s.size(), pw_id = len - 1;
+        ll ans=0LL;
+        for(int i=0;i<len;i++){
+            int val=s[i]^1;
+            if(now->node[val]!=NULL && now->node[val]->cnt > 0){
+                now = now->node[val];
+            }
+            else{
+                ans += pw[pw_id];
+                val ^= 1;
+                now = now->node[val];
+            }
+            now->cnt -= 1; pw_id -= 1;
     }
     return ans;
-}
-ll query_max(string s){
-    data *now=root;
-    int len = s.size(), pw_id = len - 1;
-    ll ans=0LL;
-    for(int i=0;i<len;i++){
-        int val=s[i]^1;
-        if(now->node[val]!=NULL && now->node[val]->cnt > 0){
-            now = now->node[val];
-        }
-        else{
-            ans += pw[pw_id];
-            val ^= 1;
-            now = now->node[val];
-        }
-        now->cnt -= 1; pw_id -= 1;
     }
-    return ans;
-}
-root=new data();///decalre this in main function;
+};
